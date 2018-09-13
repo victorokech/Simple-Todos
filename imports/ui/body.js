14237c5107ce89b8221df54ff7cbfecb919cb661
snetts <snettsblog@snetts.com>
2018-09-13 09:40:35 +0000

    Add event handler for checkbox

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -44,4 +44,7 @@ Template.body.events({
         // Clear form
         target.text.value = '';
     },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    }
 });
